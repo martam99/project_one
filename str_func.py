59a544b077b375a4e4522632a3d31a8e9b7c2e23
Mariam Tamrazyan <mart76901@gmail.com>
2023-04-25 11:53:44 +0400

    word function added2

--- str_func.py
+++ str_func.py
@@ -1,5 +1,15 @@
 def upper_word(word):
+    """ Функция, которая принимает строку и возвращает заглавными буквами"""
     return word.upper()
 
 
 upper_word(input())
+
+
+def title_word(word):
+    """Функция, которая делает заглавными первые буквы каждого слова в строке"""
+    return word.title()
+
+
+title_word(input())
+
